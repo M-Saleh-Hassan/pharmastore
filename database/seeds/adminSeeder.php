4f3fd277ea09a006d02eee0ce6d0b86ddda31d89
Mohamed Saleh <mohamad.saleh@robovics.com>
2020-05-15 18:03:43 +0200

    fix adminSeeder

--- database/seeds/adminSeeder.php
+++ database/seeds/adminSeeder.php
@@ -14,7 +14,7 @@ class adminSeeder extends Seeder
     {
         $adminRole = DB::table('role_user')->where('role_id', 1)->first();
         if(is_null($adminRole)) {
-            $user = App\Model\User::create([
+            $user = App\Models\User::create([
                 'name' => "Admin",
                 'username' => "admin",
                 'email' => "admin@pharmastore.com",
