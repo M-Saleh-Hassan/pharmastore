f79755656b6760a8463b28d29372e5a97fe57c93
Mohamed Saleh <mohamad.saleh@robovics.com>
2020-07-01 08:51:21 +0200

    add distance, store_name to orders

--- app/Http/Resources/OrderItemResource.php
+++ app/Http/Resources/OrderItemResource.php
@@ -15,8 +15,18 @@ class OrderItemResource extends JsonResource
     public function toArray($request)
     {
         $item = $this->item()->withTrashed()->first();
+
+        $lng = auth()->user()->info->lng;
+        $lat = auth()->user()->info->lat;
+        $branch = $item->branch;
+        $branchLng = $branch->lng;
+        $branchLat = $branch->lat;
+        $distance =  intVal( 6367 * acos( cos( deg2rad($lat) ) * cos( deg2rad( $branchLat ) ) * cos( deg2rad( $branchLng ) - deg2rad($lng) ) + sin( deg2rad($lat) ) * sin( deg2rad( $branchLat ) ) ) );
+        
         return [
             'item_id' => $item->id,
+            'branch_name' => $branch->name,
+            'distance' => $distance,
             'quantity' => $this->quantity,
             'basic_price' => $item->basic_price,
             'discount' => $item->discount,
