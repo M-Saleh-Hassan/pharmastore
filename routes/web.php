432d76a94eb21029baec92b8d87aefacccd9b398
Mohamed Saleh <mohamad.saleh@robovics.com>
2020-07-07 06:07:06 +0200

    Add angular frontend example

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    View::addExtension('html', 'php');
+    return View::make('index');
 });
+Route::fallback(function () {
+    View::addExtension('html', 'php');
+    return View::make('index');
+});
+
